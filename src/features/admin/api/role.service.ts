56ce439babd7834d8179075dcafc350aa5962b6e
Nghia <luutrungnghia1901@gmail.com>
2026-08-19 16:04:01 +0700

    feat: create RoleService to handle CRUD operations for admin roles

--- src/features/admin/api/role.service.ts
+++ src/features/admin/api/role.service.ts
@@ -1,9 +1,16 @@
 import { api } from '../../../services/api';
 
+export interface Permission {
+  id: number;
+  code: string;
+  description: string;
+}
+
 export interface Role {
   id: number;
   name: string;
   description: string;
+  permissions?: Permission[];
 }
 
 export interface RoleRequest {
